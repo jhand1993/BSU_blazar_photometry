222fab44ff48df5930c7812b32ea4f0228dd5a33
thelaphone <jared.hand1993@live.com>
2015-10-28 12:29:40 -0600

    modified script str to include parameter set to name of file

--- sex_auto.py
+++ sex_auto.py
@@ -4,11 +4,15 @@ import glob
 
 # Set current working directory to /Users/jaredhand/Documents/photometry/data_in/
 os.chdir('/Users/jaredhand/Documents/photometry/data_in/')
+# str for sextractor script
+script = 'sex -CATALOG_NAME %s %s'
 
 
 def get_files():
     """
     Decompress all FIT.gz files is they exists and returns list of all fit/FIT files in active directory.
+
+    :return: fits+FITS as a list
     """
     try:
         if glob.glob('*.gz*'):
@@ -24,6 +28,12 @@ files = get_files()
 
 
 def rm_spaces(list1):
+    """
+    Removes all whitespace from members of a list (assuming list contains only str objects).
+
+    :param list1: input list where are whitespace is replaced with '_'
+    :return: modified list1
+    """
     for i in list1:
         if ' ' in i:
             j = i.replace(' ', '_')
@@ -35,7 +45,12 @@ files_nospaces = rm_spaces(list1=files)
 
 
 def sex_call(list1):
-    script = 'sex %s'
+    """
+    Runs sextractor on all members of input list1.
+
+    :param list1: list of fits/FITS files in cwd
+    :return: False if subprocess fails, otherwise returns True
+    """
     print(os.getcwd())
     if os.getcwd() != '/Users/jaredhand/Documents/photometry/data_in':
         print('Check current directory.')
@@ -45,12 +60,14 @@ def sex_call(list1):
         return False
 
     for i in list1:
+        cat_name = i.split('.')[0] + '.cat'
         try:
-            sp.check_call(script % i, shell=True)
+            sp.check_call(script % (cat_name, i), shell=True)
         except:
             raise
     return True
 
+print(os.getcwd())
 sex_call(list1=files_nospaces)
 
 
